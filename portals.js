3227bfd11d4fe248299d541271fa55fd7d509799
Nick Glenn <nick@helpfulhuman.com>
2015-06-17 21:11:21 -0700

    Started working on the portal module, setting it up to work in browser or Node

---
+++ portals.js
@@ -0,0 +1,18 @@
+"use strict";
+
+(function() {
+  var root = this;
+
+
+
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = portals
+    }
+    exports.portals = portals
+  }
+  else {
+    root.portals = portals
+  }
+
+}).call(this)
