6d186ebd87659f8c2fdf08e160b94743d943b99c
Nick Glenn <nick@helpfulhuman.com>
2015-06-17 23:32:49 -0700

    Adding additional test for send() method

--- test/portal/send.js
+++ test/portal/send.js
@@ -23,7 +23,7 @@ describe('Portal#send()', function () {
   /**
    * @test
    */
-  it('sends a simple GET request', function (done) {
+  it('sends a simple request with success', function (done) {
     var opts = { method: 'GET', url: '/' };
     var data = JSON.stringify({ foo: 'bar' });
     var promise = web.send(opts);
@@ -35,6 +35,25 @@ describe('Portal#send()', function () {
     promise.then(function (res) {
       expect(res.status).to.equal(200);
       expect(res.body).to.equal(data);
+      done();
+    });
+  });
+
+  /**
+   * @test
+   */
+  it('sends a simple request with failure', function (done) {
+    var opts = { method: 'POST', url: '/', body: 'hey' };
+    var data = JSON.stringify({ foo: 'bar' });
+    var promise = web.send(opts);
+
+    // set up the fake response
+    this.requests[0].respond(402, {}, data);
+
+    // proceed with promise resolution
+    promise.catch(function (res) {
+      expect(res.status).to.equal(402);
+      expect(res.body).to.equal(opts.body);
       done();
     });
   });
