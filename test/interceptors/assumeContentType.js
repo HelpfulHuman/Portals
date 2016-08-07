abf6c9049c154516201ae45f3a315aa401b7533a
Nick Glenn <nick@helpfulhuman.com>
2016-08-07 01:10:08 -0700

    Updated test for assumeContentType interceptor

--- test/interceptors/assumeContentType.js
+++ test/interceptors/assumeContentType.js
@@ -3,10 +3,34 @@ const { interceptors: { assumeContentType } } = require('../../');
 
 describe('interceptors/assumeContentType', function () {
 
-  it('leaves the Content-Type header alone if it doesn\'t know how to resolve it');
+  before(function () {
+    global.FormData = function FormData () {}
+  });
 
-  it('sets the Content-Type header to multipart/form-data if the request body is a FormData instance');
+  after(function () {
+    delete global.FormData;
+  });
 
-  it('sets the Content-Type header to application/xml if the body is a string that starts with "<"');
+  it('leaves the Content-Type header alone if it doesn\'t know how to resolve it', function () {
+    const input = { headers: {}, body: '' };
+    const output = assumeContentType(input);
+
+    expect(output).to.equal(input);
+  });
+
+  it('sets the Content-Type header to multipart/form-data if the request body is a FormData instance', function () {
+    const form = new FormData();
+    const input = { headers: {}, body: form };
+    const output = assumeContentType(input);
+
+    expect(output.headers['Content-Type']).to.equal('multipart/form-data');
+  });
+
+  it('sets the Content-Type header to application/xml if the body is a string that starts with "<"', function () {
+    const input = { headers: {}, body: '<?xml>' };
+    const output = assumeContentType(input);
+
+    expect(output.headers['Content-Type']).to.equal('application/xml');
+  });
 
 });
