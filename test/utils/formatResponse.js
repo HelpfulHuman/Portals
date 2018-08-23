88157299cab9b5005eeb9bc569dec24bec024e5a
Nicholas Glenn <nick@helpfulhuman.com>
2018-08-23 14:30:13 -0700

    Removing old, dead code

--- test/utils/formatResponse.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require('chai');
-const { utils: { formatResponse } } = require('../../');
-
-describe('utils/formatResponse', function () {
-
-  it('converts the given XHR object into a simple object literal with status, body, type, and standard headers');
-
-});
